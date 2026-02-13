980a6c94ce8f8bf8580d29f210456570b771741c
Darlan0786 <darlanana06@gmail.com>
2026-02-13 01:08:57 -0300

    Implement keyboard input management functions

--- inputManager.js
+++ inputManager.js
@@ -1,1 +1,15 @@
+// inputManager.js
+// Gerencia teclado e mouse
 
+const keys = {};
+
+window.addEventListener('keydown', (e) => { keys[e.code] = true; });
+window.addEventListener('keyup', (e) => { keys[e.code] = false; });
+
+export function isKeyDown(keyCode) {
+    return !!keys[keyCode];
+}
+
+export function getKeys() {
+    return keys;
+}
